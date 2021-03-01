0894fc884da0e984bf80e45394a8f103d4baf728
Zanmatox <cheungkevin@hotmail.fr>
2021-03-01 22:46:19 +0100

    Login form

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -12,7 +12,9 @@ class LoginPage extends StatelessWidget{
         child: Column(
           children: <Widget>[
             _buildHeader(),
+            SizedBox(height: 15),
             _buildTitle(),
+            _buildForm(),
           ],
         ),
       ),
@@ -23,8 +25,9 @@ Widget _buildHeader() {
   return new Container(
     child: Image.network("http://www.cerfi.ch/Htdocs/Images/Pictures/puid_e2ad9412-cca4-4eaf-961c-570cf0f342eb_3354.jpg"),
   );
-}
+} 
 
+//Création du titre
 Widget _buildTitle() {
   return new Center(
     child:Text(
@@ -33,4 +36,51 @@ Widget _buildTitle() {
       ),
   );
 }
+//Création formulaire
+Widget _buildForm() {
+  return new Container(
+    // Padding
+    padding: EdgeInsets.all(15),
+    child: new Column(
+      children: <Widget>[
+        new Container(
+          child: TextFormField(
+            // TODO : Gérer le contenu du form
+            decoration: InputDecoration(
+              labelText: "Votre émail : "
+            ),
+          ),
+          ),
+          SizedBox(height: 10),
+          new Container(
+          child: TextFormField(
+            // TODO : Gérer le contenu du form
+            decoration: InputDecoration(
+              labelText: "Votre mot de passe : "
+            ),
+            obscureText: true,
+          ),
+          ),
+          SizedBox(height: 15),
+          new Container(
+            child: RaisedButton(
+              child: Text("Login"),
+              color: Colors.red, 
+              onPressed: () {
+                // TODO : Gérer le click
+              },
+              ),
+          ),
+          new Container(
+            child: FlatButton(
+              child: Text("Mot de passe oublié ?"),
+              onPressed: () {
+                // TODO : a gerer
+              },
+              ),
+          )
+      ],
+    )
+  );
+} 
 }
