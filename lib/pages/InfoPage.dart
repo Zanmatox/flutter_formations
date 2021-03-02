9f0310ba323dfb24a7c30872d21cc7b12f9b53cf
Zanmatox <cheungkevin@hotmail.fr>
2021-03-02 15:20:51 +0100

    Ajout infos + avis

--- lib/pages/InfoPage.dart
+++ lib/pages/InfoPage.dart
@@ -12,10 +12,83 @@ class InfosPage extends StatefulWidget {
 }
 
 class _InfosPageState extends State<InfosPage> {
+
+int selectedTab = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Infos de " + widget.cours.name),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Détails du cours"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            _buildHeader(),
+            SizedBox(height: 15),
+            _buildInfos(),
+            SizedBox(height: 15),
+            _buildBtn(),
+
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedTab,
+        onTap: (int index) {
+          setState(() {
+            selectedTab = index;
+          });
+        },
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.play_circle_outline),
+            title: Text("Infos")
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            title: Text("Avis")
+          ),
+          
+        ],
+      ),
     );
   }
+
+  Widget _buildHeader() {
+    return new Container(
+      child: Image.asset(widget.cours.image),
+    );
+  }
+
+    Widget _buildInfos() {
+    return new Container(
+      child: Column(
+        children: <Widget>[
+          Text(
+            widget.cours.name,
+            style: TextStyle(fontSize: 26),
+          ),
+          SizedBox(height: 15),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(widget.cours.description),
+          ),
+          SizedBox(height: 15),
+          Text("19.99 €")
+        ],)
+    );
+  }
+    Widget _buildBtn() {
+    return new Container(
+      child: RaisedButton(
+        onPressed: () {
+          log("TODO : Bouton acheter");
+        },
+        child: Text("ACHETER"),
+      ),
+    );
+  }
+
 }
